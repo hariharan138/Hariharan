9742ad0b026fe547724281b7e6a53a6cf75a4047
HariHaran <101501297+hariharan138@users.noreply.github.com>
2024-12-24 23:27:39 +0530

    hi

--- contact/mail.js
+++ contact/mail.js
@@ -64,7 +64,7 @@ let server = http.createServer((req, res) => {
         }
     } else {
         res.writeHead(200, { "Content-Type": "text/html" });
-        fs.createReadStream("./contactme.html", "utf-8").pipe(res);
+        fs.createReadStream("./contact/contactme.html", "utf-8").pipe(res);
     }
 });
 
